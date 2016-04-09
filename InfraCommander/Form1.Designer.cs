dd365170fcba4f8aca938e8e5fb2de2936821cdf
RaghuTeja A <a.raghu.teja@gmail.com>
2016-04-09 20:47:02 +0530

    Icon Changes

--- InfraCommander/Form1.Designer.cs
+++ InfraCommander/Form1.Designer.cs
@@ -52,18 +52,20 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Location = new System.Drawing.Point(16, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(424, 21);
+            this.label1.Size = new System.Drawing.Size(532, 27);
             this.label1.TabIndex = 0;
             this.label1.Text = "Turn ON and Point a remote and Press a button";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // buttonStart
             // 
-            this.buttonStart.Location = new System.Drawing.Point(60, 80);
+            this.buttonStart.Location = new System.Drawing.Point(80, 98);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.Size = new System.Drawing.Size(100, 28);
             this.buttonStart.TabIndex = 1;
             this.buttonStart.Text = "Turn ON";
             this.buttonStart.UseVisualStyleBackColor = true;
@@ -75,9 +77,10 @@
             // 
             // buttonSto
             // 
-            this.buttonSto.Location = new System.Drawing.Point(315, 80);
+            this.buttonSto.Location = new System.Drawing.Point(420, 98);
+            this.buttonSto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonSto.Name = "buttonSto";
-            this.buttonSto.Size = new System.Drawing.Size(75, 23);
+            this.buttonSto.Size = new System.Drawing.Size(100, 28);
             this.buttonSto.TabIndex = 2;
             this.buttonSto.Text = "Turn OFF";
             this.buttonSto.UseVisualStyleBackColor = true;
@@ -86,23 +89,24 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(146, 137);
+            this.label2.Location = new System.Drawing.Point(195, 169);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.Size = new System.Drawing.Size(36, 17);
             this.label2.TabIndex = 4;
             this.label2.Text = "       ";
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(442, 206);
+            this.ClientSize = new System.Drawing.Size(589, 254);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.buttonSto);
             this.Controls.Add(this.buttonStart);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MainForm";
             this.Text = "InfraCommander";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
