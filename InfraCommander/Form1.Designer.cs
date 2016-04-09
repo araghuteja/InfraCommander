f01e6ba1dfc34e93252ef992bfa5d11cb871db8e
RaghuTeja A <a.raghu.teja@gmail.com>
2016-04-09 20:42:43 +0530

    Initial Code

---
+++ InfraCommander/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace InfraCommander
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.InfraCommander = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.buttonSto = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // InfraCommander
+            // 
+            this.InfraCommander.Icon = ((System.Drawing.Icon)(resources.GetObject("InfraCommander.Icon")));
+            this.InfraCommander.Text = "InfraCommander";
+            this.InfraCommander.Visible = true;
+            this.InfraCommander.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.InfraCommander_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(424, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Turn ON and Point a remote and Press a button";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(60, 80);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Turn ON";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // buttonSto
+            // 
+            this.buttonSto.Location = new System.Drawing.Point(315, 80);
+            this.buttonSto.Name = "buttonSto";
+            this.buttonSto.Size = new System.Drawing.Size(75, 23);
+            this.buttonSto.TabIndex = 2;
+            this.buttonSto.Text = "Turn OFF";
+            this.buttonSto.UseVisualStyleBackColor = true;
+            this.buttonSto.Click += new System.EventHandler(this.buttonSto_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(146, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "       ";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 206);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonSto);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "MainForm";
+            this.Text = "InfraCommander";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Resize += new System.EventHandler(this.MainForm_Resize_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon InfraCommander;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button buttonSto;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ InfraCommander/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InfraCommander
+{
+    public partial class MainForm : Form
+    {
+        String RxString1;
+        String StringCMD;
+        String StrCom;
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int APPCOMMAND_VOLUME_UP = 0xA0000;
+        private const int APPCOMMAND_VOLUME_DOWN = 0x90000;
+        private const int WM_APPCOMMAND = 0x319;
+
+         [DllImport("user32.dll")]
+         static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        
+        private void MainForm_Resize_1(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == WindowState)
+            {
+                Hide();
+            }
+         
+        }
+
+        private void InfraCommander_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = "COM7";
+            serialPort1.BaudRate = 9600;
+
+            serialPort1.Open();
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    buttonStart.Enabled = false;
+                    buttonSto.Enabled = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                //Log.write("UnauthorizedAccessException");
+            }
+           
+            
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            RxString1 = serialPort1.ReadLine();
+            if(!RxString1.Contains("FFFF"))
+           StringCMD = RxString1;
+            this.Invoke(new EventHandler(DisplayText));
+            Commander(StringCMD);
+
+        }
+
+        private void DisplayText(object sender, EventArgs e)
+        {
+            //textBox1.Text = RxString;
+            label2.Text = RxString1;
+        }
+
+        private void Commander(String com)
+        {
+            Console.WriteLine(com.Length);
+            try
+            {
+                if (com.Length > 4)
+                {
+                    Console.WriteLine(com.Remove(6, 1));
+                    StrCom = com.Remove(6, 1);
+
+                    switch (StrCom)
+                    {
+                        case "BD906F":
+                            Console.WriteLine("Sound");
+                            Process.Start("I:\\Software\\PlaybackDevice\\PlaybackDevice.exe");
+                            break;
+                        case "BD30CF":
+                            keybd_event((byte)Keys.VolumeUp, 0, 0, 0); // increase volume
+                            break;
+                        case "BD08F7":
+                            keybd_event((byte)Keys.VolumeDown, 0, 0, 0); // increase volume
+                            break;
+                        case "BD20DF":
+                            keybd_event((byte)Keys.VolumeMute, 0, 0, 0); // increase volume
+
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen) serialPort1.Close();
+        }
+
+        private void buttonSto_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                buttonStart.Enabled = true;
+                buttonSto.Enabled = false;
+            }
+        }
+    }
+}
